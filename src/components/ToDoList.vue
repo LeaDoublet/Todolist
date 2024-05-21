22ed39390d93b5a10cd6e248fc1f4f6eb4591839
Léa Doublet <leadbtcode@gmail.com>
2024-05-21 09:49:35 +0200

    Initial commit

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Chose from "../Chose"
+
+
+const listeC = reactive([
+    new Chose("menage"), 
+    new Chose("Vaiselle")
+    ]);
+
+</script>
+<template>
+    <h3>
+        Liste des choses à faire
+    </h3>
+    <ul>
+        <li v-for="chose in listeC" :key="chose.id">
+            {{ chose.pourAfficher() }}
+        </li>
+    </ul>
+</template>
